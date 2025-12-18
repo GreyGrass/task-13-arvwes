0367052ed8507276b3944f6288548bb346407bba
Arvid West <117587210+GreyGrass@users.noreply.github.com>
2025-12-18 11:06:16 +0100

    rays can bounce now, and scatter on matt materials

--- src/main.rs
+++ src/main.rs
@@ -12,37 +12,53 @@ use std::f64::INFINITY;
 use std::fs::File;
 use std::io::Write;
 
+fn random_in_unit_sphere() -> Vec3 {
+    let mut rng = rand::rng();
+    loop {
+        let p = Vec3 {
+            x: rng.random_range(-1.0..1.0) as f64,
+            y: rng.random_range(-1.0..1.0) as f64,
+            z: rng.random_range(-1.0..1.0) as f64,
+        };
+        if p.length_squared() <= 1.0 {
+            return p;
+            
+        }
+    }
+}
 
 fn color(ray: Ray, hitables: &World) -> Vec3 {
-    let rec = hitables.hit_anything(0.0, INFINITY, &ray);
+    let rec = hitables.hit_anything(0.001, INFINITY, &ray);
     if rec.is_some() {
         let rec = rec.unwrap();
-        return Vec3 {
-            x: rec.normal.x + 1.0,
-            y: rec.normal.y + 1.0,
-            z: rec.normal.z + 1.0,
-        } * 0.5;
+        let target = rec.p + rec.normal + random_in_unit_sphere();
+        let bounce = Ray {
+            a: rec.p,
+            b: target,
+        };
+        return color(bounce, &hitables) * 0.5;
+    } else {
+        let u_direction = ray.direction().unit_vector();
+        let t: f64 = 0.5 * (u_direction.y + 1.0);
+        let start_value = Vec3 {
+            x: 1.0,
+            y: 1.0,
+            z: 1.0,
+        };
+        let end_value = Vec3 {
+            x: 0.5,
+            y: 0.7,
+            z: 1.0,
+        };
+        start_value * (1.0 - t) + end_value * t
     }
-    let u_direction = ray.direction().unit_vector();
-    let t: f64 = 0.5 * (u_direction.y + 1.0);
-    let start_value = Vec3 {
-        x: 1.0,
-        y: 1.0,
-        z: 1.0,
-    };
-    let end_value = Vec3 {
-        x: 0.5,
-        y: 0.7,
-        z: 1.0,
-    };
-    start_value * (1.0 - t) + end_value * t
 }
 
 fn main() -> std::io::Result<()> {
     let mut file = File::create("image.ppm")?;
 
-    let nx = 200;
-    let ny = 100;
+    let nx = 1280;
+    let ny = 720;
     let ns = 100;
     let nxf = nx as f64;
     let nyf = ny as f64;
@@ -89,6 +105,7 @@ fn main() -> std::io::Result<()> {
                 col += color(ray, &world);
             }
             col /= nsf;
+            col = Vec3{x: col.x.sqrt(), y: col.y.sqrt(), z: col.z.sqrt()};
             let ir: u8 = (255.99 * col.x) as u8;
             let ig: u8 = (255.99 * col.y) as u8;
             let ib: u8 = (255.99 * col.z) as u8;
